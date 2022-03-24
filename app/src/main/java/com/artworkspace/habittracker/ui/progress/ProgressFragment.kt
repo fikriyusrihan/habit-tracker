2e0cfb7eb9eeb295d6d96964c2520cefe5d748bc
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-03-24 11:28:43 +0700

    implement progress layout

--- app/src/main/java/com/artworkspace/habittracker/ui/progress/ProgressFragment.kt
+++ app/src/main/java/com/artworkspace/habittracker/ui/progress/ProgressFragment.kt
@@ -1,12 +1,16 @@
 package com.artworkspace.habittracker.ui.progress
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import com.artworkspace.habittracker.R
 import com.artworkspace.habittracker.databinding.FragmentProgressBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ProgressFragment : Fragment() {
 
@@ -15,23 +19,45 @@ class ProgressFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val progressViewModel by viewModels<ProgressViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(ProgressViewModel::class.java)
-
         _binding = FragmentProgressBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnShowInformation.setOnClickListener {
+                showProgressInformation(requireContext())
+            }
+
+            cardStreak.setOnLongClickListener {
+                Toast.makeText(requireContext(), getString(R.string.keep_it_up), Toast.LENGTH_SHORT)
+                    .show()
+                true
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun showProgressInformation(ctx: Context) {
+        MaterialAlertDialogBuilder(ctx)
+            .setTitle(getString(R.string.progress_information_title))
+            .setMessage(getString(R.string.progress_information_message))
+            .setPositiveButton(getString(R.string.progress_information_ok)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
