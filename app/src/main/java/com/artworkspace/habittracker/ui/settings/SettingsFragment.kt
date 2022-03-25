9bd9c2f2e4353cd68d0f312d526032141632981f
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-03-26 06:05:26 +0700

    implement share and send feedback

--- app/src/main/java/com/artworkspace/habittracker/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/artworkspace/habittracker/ui/settings/SettingsFragment.kt
@@ -1,14 +1,19 @@
 package com.artworkspace.habittracker.ui.settings
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.artworkspace.habittracker.R
 import com.artworkspace.habittracker.databinding.FragmentSettingsBinding
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentSettingsBinding? = null
     private val settingsViewModel: SettingsViewModel by viewModels()
@@ -26,8 +31,77 @@ class SettingsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnSendFeedback.setOnClickListener(this@SettingsFragment)
+            btnShareApp.setOnClickListener(this@SettingsFragment)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            binding.btnSendFeedback.id -> {
+                composeEmail(
+                    addresses = arrayOf(EMAIL),
+                    subject = getString(R.string.mail_subject),
+                    body = getString(R.string.feedback_mail_body)
+                )
+            }
+            binding.btnShareApp.id -> {
+                shareApp(getString(R.string.share_app_message))
+            }
+        }
+    }
+
+    /**
+     * Create an Intent to share message
+     *
+     * @param message a message to share
+     */
+    private fun shareApp(message: String) {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            putExtra(Intent.EXTRA_TEXT, message)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(intent, null)
+        startActivity(shareIntent)
+    }
+
+    /**
+     * Create an Intent to send email for feedback to developer
+     *
+     * @param addresses receiver's email address
+     * @param subject email's subject
+     * @param body email's body
+     */
+    private fun composeEmail(addresses: Array<String>, subject: String, body: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, addresses)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, body)
+        }
+
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.application_not_found),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    companion object {
+        private const val EMAIL = "fikriyusrihan@gmail.com"
     }
 }
