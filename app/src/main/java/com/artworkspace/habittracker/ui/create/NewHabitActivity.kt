ad302b08a86739a2dc333c3d3555fc6544a70f27
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-03-25 23:16:08 +0700

    implement startAt timestamp

--- app/src/main/java/com/artworkspace/habittracker/ui/create/NewHabitActivity.kt
+++ app/src/main/java/com/artworkspace/habittracker/ui/create/NewHabitActivity.kt
@@ -1,36 +1,82 @@
 package com.artworkspace.habittracker.ui.create
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.artworkspace.habittracker.R
 import com.artworkspace.habittracker.data.entity.Habit
 import com.artworkspace.habittracker.databinding.ActivityNewHabitBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
 import java.util.*
 
 @AndroidEntryPoint
-class NewHabitActivity : AppCompatActivity() {
+class NewHabitActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityNewHabitBinding
+
+    private var startAtTimestamp: Long? = null
+
     private val viewModel: NewHabitViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewHabitBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+
+        viewModel.startAtTimestamp.observe(this) { timeInMillis ->
+            startAtTimestamp = timeInMillis
+            setDefaultStartAt(timeInMillis)
+        }
 
         binding.apply {
-            btnExit.setOnClickListener {
+            btnExit.setOnClickListener(this@NewHabitActivity)
+            btnSave.setOnClickListener(this@NewHabitActivity)
+            startFromStatus.setOnClickListener(this@NewHabitActivity)
+        }
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            binding.btnExit.id -> {
                 finish()
             }
 
-            btnSave.setOnClickListener {
-                Toast.makeText(this@NewHabitActivity, "It clicked", Toast.LENGTH_SHORT).show()
+            binding.btnSave.id -> {
+                Toast.makeText(
+                    this@NewHabitActivity,
+                    getString(R.string.new_habit_created),
+                    Toast.LENGTH_SHORT
+                ).show()
                 saveNewHabit()
                 finish()
             }
+
+            binding.startFromStatus.id -> {
+                setStartDate()
+            }
+        }
+    }
+
+    private fun setDefaultStartAt(timeInMillis: Long) {
+        val date = Date(timeInMillis)
+        val sdf = SimpleDateFormat.getDateInstance()
+
+        binding.tvStartFromStatus.text = getString(R.string.start_from, sdf.format(date))
+    }
+
+    private fun setStartDate() {
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText(getString(R.string.select_date))
+            .setSelection(startAtTimestamp)
+            .build()
+
+        datePicker.show(supportFragmentManager, TAG)
+        datePicker.addOnPositiveButtonClickListener {
+            viewModel.setStartAtTimestamp(it)
         }
     }
 
@@ -39,13 +85,17 @@ class NewHabitActivity : AppCompatActivity() {
         val name = binding.etHabitTitle.text.toString()
         val icon = null
         val description = ""
-        val startAt = Calendar.getInstance().timeInMillis
+        val startAt = startAtTimestamp
         val createdAt = Calendar.getInstance().timeInMillis
 
         val habit = Habit(
-            id, name, icon, description, startAt, createdAt
+            id, name, icon, description, startAt!!, createdAt
         )
 
         viewModel.insertHabit(habit)
+    }
+
+    companion object {
+        private const val TAG = "NewHabitActivity"
     }
 }
--- app/src/main/java/com/artworkspace/habittracker/ui/create/NewHabitViewModel.kt
+++ app/src/main/java/com/artworkspace/habittracker/ui/create/NewHabitViewModel.kt
@@ -1,11 +1,14 @@
 package com.artworkspace.habittracker.ui.create
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.artworkspace.habittracker.data.HabitRepository
 import com.artworkspace.habittracker.data.entity.Habit
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,9 +16,16 @@ class NewHabitViewModel @Inject constructor(
     private val habitRepository: HabitRepository
 ) : ViewModel() {
 
+    private var _startAtTimestamp = MutableLiveData(Calendar.getInstance().timeInMillis)
+    val startAtTimestamp: LiveData<Long> = _startAtTimestamp
+
     fun insertHabit(habit: Habit) {
         viewModelScope.launch {
             habitRepository.insertHabit(habit)
         }
+    }
+
+    fun setStartAtTimestamp(timeInMillis: Long) {
+        _startAtTimestamp.value = timeInMillis
     }
 }
