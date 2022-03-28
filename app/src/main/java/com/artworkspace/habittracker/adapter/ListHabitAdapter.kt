3fd10229a4509a85d0cca024e0be7b17c23bb183
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-03-28 16:46:26 +0700

    implement swipe gesture on recyclerview

--- app/src/main/java/com/artworkspace/habittracker/adapter/ListHabitAdapter.kt
+++ app/src/main/java/com/artworkspace/habittracker/adapter/ListHabitAdapter.kt
@@ -1,11 +1,18 @@
 package com.artworkspace.habittracker.adapter
 
+import android.content.Context
+import android.graphics.Canvas
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.artworkspace.habittracker.BaseApplication
+import com.artworkspace.habittracker.R
 import com.artworkspace.habittracker.data.entity.Habit
 import com.artworkspace.habittracker.databinding.HabitItemBinding
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
+
 
 class ListHabitAdapter(
     private val listHabit: List<Habit>,
@@ -19,6 +26,8 @@ class ListHabitAdapter(
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
         this.onItemClickCallback = onItemClickCallback
     }
+
+    fun getHabitItem(index: Int): Habit = listHabit[index]
 
     class ListViewHolder(var binding: HabitItemBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -43,5 +52,51 @@ class ListHabitAdapter(
 
     interface OnItemClickCallback {
         fun onItemClicked(habit: Habit)
+    }
+
+    abstract class SwipeGesture(context: Context) :
+        ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+        private val doneColor = ContextCompat.getColor(context, R.color.blue_500)
+        private val skipColor = ContextCompat.getColor(context, R.color.purple_500_half)
+        private val doneIcon = R.drawable.ic_baseline_check_white_24
+        private val skipIcon = R.drawable.ic_baseline_arrow_forward_24
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onChildDraw(
+            c: Canvas,
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            dX: Float,
+            dY: Float,
+            actionState: Int,
+            isCurrentlyActive: Boolean
+        ) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+
+            RecyclerViewSwipeDecorator.Builder(
+                c,
+                recyclerView,
+                viewHolder,
+                dX,
+                dY,
+                actionState,
+                isCurrentlyActive
+            )
+                .addSwipeLeftBackgroundColor(skipColor)
+                .addSwipeLeftActionIcon(skipIcon)
+                .addSwipeRightBackgroundColor(doneColor)
+                .addSwipeRightActionIcon(doneIcon)
+                .create()
+                .decorate()
+        }
+
     }
 }
--- app/src/main/java/com/artworkspace/habittracker/ui/journal/JournalFragment.kt
+++ app/src/main/java/com/artworkspace/habittracker/ui/journal/JournalFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.artworkspace.habittracker.BaseApplication
 import com.artworkspace.habittracker.adapter.ListHabitAdapter
 import com.artworkspace.habittracker.data.entity.Habit
@@ -61,6 +63,32 @@ class JournalFragment : Fragment() {
     private fun showUncompletedHabit(habits: List<Habit>) {
         val linearLayoutManager = LinearLayoutManager(requireContext())
         val listAdapter = ListHabitAdapter(habits, requireActivity().application as BaseApplication)
+        val swipeGesture = object : ListHabitAdapter.SwipeGesture(requireContext()) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                when (direction) {
+                    ItemTouchHelper.LEFT -> {
+                        val habit = listAdapter.getHabitItem(viewHolder.absoluteAdapterPosition)
+                        Toast.makeText(
+                            requireContext(),
+                            habit.name,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    ItemTouchHelper.RIGHT -> {
+                        Toast.makeText(
+                            requireContext(),
+                            "${viewHolder.absoluteAdapterPosition}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+
+        ItemTouchHelper(swipeGesture).apply {
+            attachToRecyclerView(binding.rvHabitNotCompleted)
+        }
 
         binding.rvHabitNotCompleted.apply {
             layoutManager = linearLayoutManager
